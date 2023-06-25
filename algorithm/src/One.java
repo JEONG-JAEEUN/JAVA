0299f86b0a8e31bbd121aabafbb681a54c584dee
정재은 <jjyjjy0528@naver.com>
2023-06-26 02:24:59 +0900

    구구단

--- algorithm/src/One.java
+++ algorithm/src/One.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class One {
     public static void main(String[] args) {
-
+        Scanner a = new Scanner(System.in);
+        int i = a.nextInt();
+        for(int j=1;j<=9;j++){
+            System.out.println(i +" * "+j+" = "+i*j);
+        }
     }
 }
